96a007611c43be71ece8781ddcbcbb8f53b2c094
Shlok Rastogi <shlokrastogi2004@gmail.com>
2026-03-25 20:13:58 +0530

    making the routes

--- src/app.js
+++ src/app.js
@@ -2,8 +2,24 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!!");
+app.get("/user", (req, res) => {
+  res.json({ name: "John Doe", age: 30 });
+});
+
+app.post("/user", (req, res) => {
+  res.json({ message: "User created successfully" });
+});
+
+app.patch("/user/:id", (req, res) => {
+  res.json({ message: `User with id ${req.params.id} updated successfully` });
+});
+
+app.put("/user/:id", (req, res) => {
+  res.json({ message: `User with id ${req.params.id} updated successfully` });
+});
+
+app.delete("/user/:id", (req, res) => {
+  res.json({ message: `User with id ${req.params.id} deleted successfully` });
 });
 
 app.listen(3000, () => {
